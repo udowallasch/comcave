ff82b65c423aac925fa49a21adc885df84111aa2
udowallasch <uwr05@t-online.de>
2019-02-09 14:21:52 +0100

    9-2 3

--- Task_8.2/Task_8.2.cpp
+++ Task_8.2/Task_8.2.cpp
@@ -11,7 +11,7 @@ using std::cout;
 int main()
 {
 //Teilnehmer::Teilnehmer(Mensch m, int k, Datum e, Datum a, bool c, std::string t)
-Teilnehmer t1{Mensch{Name{"Udo","Wallasch"}, Mensch::geschlecht::M, Datum{1,Datum::Nov,58}}, 4711, Datum{8,Datum::Jan,2019}, Datum{6,Datum::Mar,2019}, false, "ARGE"};
+Teilnehmer t1{Mensch{Name{"Bruno","Mustermann"}, Mensch::geschlecht::M, Datum{1,Datum::Apr,1911}}, 4711, Datum{8,Datum::Jan,2019}, Datum{6,Datum::Mar,2019}, false, "ARGE"};
 
 cout <<t1.getName();
 cout << "\n" << t1.getGeburtstag().toString(dotDat{});
@@ -19,7 +19,7 @@ cout << "\n" << t1.getGeburtstag().toString(slashDat{});
 cout << "\n" << t1.getGeburtstag().toString(isoDat{});
 
 //Adresse::Adresse(std::string l, std::string p, std::string g, std::string s, std::string h)
-t1.setAdresse(Adresse{ "D","13407","Berlin","Alt Reinickendorf", "29e" });
+t1.setAdresse(Adresse{ "D","112233","MusterStadt","Musterstrasse", "13" });
 
 cout << "\nAdresse: " << t1.getAdresse().toString();
 
